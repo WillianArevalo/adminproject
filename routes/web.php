d5ecf4d01edd0120ffcb2f683db57cc710f5d0f7
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-20 18:50:16 -0600

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->group(function () {
+    Route::get("/", "index")->name("home");
+});
+
+Route::controller(ProjectController::class)->group(function () {
+    Route::get("/projects", "index")->name("projects.index");
+    Route::get("/projects/create", "create")->name("projects.create");
+    Route::post("/projects/store", "store")->name("projects.store");
+});
+
+
+Route::controller(LanguageController::class)->group(function () {
+    Route::get("/languages", "index")->name("languages");
+    Route::post("/languages/store", "store")->name("languages.store");
 });
