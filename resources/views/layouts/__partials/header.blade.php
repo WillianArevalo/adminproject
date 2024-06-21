e02f567fa77052eaebfcb59c6666fc6d5ab460e4
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-20 18:46:13 -0600

    add template and header partial

---
+++ resources/views/layouts/__partials/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="flex items-center justify-start gap-2 bg-violet-800 bg-opacity-10 p-3 rounded">
+        <x-icon icon="folder" class="w-7 h-7 text-violet-400" />
+        <h1 class="text-2xl font-bold text-violet-400 font-spaceGrotesk uppercase">Administrador de proyectos</h1>
+    </div>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+    @vite('resources/css/checkbox.css')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+</head>
+
+<body class="w-3/5 mx-auto bg-black mt-4">
+    @include('layouts.__partials.header')
+    @include('layouts.__partials.alert')
+    @yield('content')
+</body>
+@vite('resources/js/app.js')
+
+</html>
